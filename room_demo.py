e8c91ca1216bc5d2329d5fc02db44eefa1bb52dd
landpack <ada.frank.ak@gmail.com>
2016-11-08 10:12:44 +0800

    [fixed] some bug

--- room_demo.py
+++ room_demo.py
@@ -3,7 +3,6 @@ class RoomManager(object):
     """
     Each run has 6 key
     """
-    #keys_set = []
     
     fragmentary_room = {}
     check_table = {}
@@ -41,7 +40,7 @@ class RoomManager(object):
         for key_level in key_levels:
             room_names = self.fragmentary_room.get(key_level)
             if room_names:
-                room_name = room_names.pop()
+                room_name = self.fragmentary_room.get(key_level).pop()
                 if key_level > 1:
                     self.fragmentary_room(key - 1).push(room_name)
                 return room_name
@@ -49,24 +48,25 @@ class RoomManager(object):
 
     
     def new_room_name(self, prefix='room', subfix=''):
-        index = RoomManager.index
+        index = self.index
         return '%s%s%s' % ( prefix, index, subfix)
 
     
     def new_room(self, room_name, uid):
-        if RoomManager.current_counter == 0:
+        if self.current_counter == 0:
             room_name = self.new_room_name()
             self.game_rooms[room_name] = [uid, ]
-            RoomManager.current_counter = RoomManager.current_counter + 1
-        elif RoomManager.current_counter <= self.size:
+            self.current_counter = self.current_counter + 1
+        elif self.current_counter <= self.size:
             room_name = self.new_room_name()
             self.game_rooms[room_name].append(uid)
-            RoomManager.current_counter = RoomManager.current_counter + 1
+            self.current_counter = self.current_counter + 1
         else:
-            RoomManager.current_counter = 0
-            RoomManager.index = RoomManager.index + 1
+            self.current_counter = 0
+            self.index = self.index + 1
             room_name = self.new_room_name()
             self.game_rooms[room_name] = [uid, ]
+            self.current_counter = self.current_counter + 1
 
         self.check_table[uid] = room_name
 
@@ -85,15 +85,30 @@ class RoomManager(object):
 
 
     def check_out(self, uid):
-        room_name = self.check_table[uid]
-        self.game_rooms[room_name].remove(uid)
+        try:
+            room_name = self.check_table[uid]
+        except Exception as ex:
+            raise KeyError('check_table has no such key [%s]' % uid)
+
+        try:
+            self.game_rooms[room_name].remove(uid)
+        except Exception as ex:
+            raise KeyError('room [%s] have no found any memeber name as [%s]' % (room_name, uid))
+
         residue = self.game_rooms[room_name]
-        if residue == 0:
+        # residue is room list! Example : ['room1', 'roomm2']
+
+        if len(residue) == 0:
             #drop the room
-            pass
-        else:
+            room_level = self.size - len(residue) - 1
+            del self.game_rooms[room_name]
+            self.fragmentary_room[room_level].remove(room_name)
+        elif len(residue) > 0:
             room_level = self.size - len(residue)
             self.fragmentary_room[room_level].append(room_name)
+        else:
+            pass
+
         del self.check_table[uid]
 
 
@@ -107,11 +122,32 @@ if __name__ == '__main__':
     manager.check_in(12399)
     manager.check_in(12300)
     print manager.game_rooms
+    # test check out
+    print '=' * 80
     manager.check_out(12388)
+    print 'manager.check_out(12388)'
     print manager.game_rooms
     manager.check_in(12888)
+    print 'manager.check_in(12888)'
     print manager.game_rooms
+
+    print 'manager.check_in(22299)'
     manager.check_in(22299)
     print manager.game_rooms
+    print 'manager.check_in(88800)'
     manager.check_in(88800)
     print manager.game_rooms
+    # retrieve room test
+    print '=' * 80
+    manager.check_out(12344)
+    manager.check_out(12355)
+    manager.check_out(12366)
+    manager.check_out(12399)
+    manager.check_out(12300)
+    print manager.game_rooms
+    # drop empty room test
+    print '=' * 80
+    print 'manager.check_out(12888)'
+    manager.check_out(12888)
+    print manager.game_rooms
+
